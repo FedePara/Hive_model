38682257275fb6ca22ac17650f8fade5f47c0817
FedePara <f.paradiso.northumbria.ac.uk>
2024-02-13 13:42:04 +0100

    R script for analysis Scenario1

---
+++ Scenario 1.R
@@ -0,0 +1,112 @@
+##############
+### Scenario 1 
+##############
+
+# required libraries
+library(sf)
+library (ggplot2)
+library(dplyr)
+
+##############
+### Import data
+##############
+
+## Read the polygon files
+city_border<- st_read("Data/Newcastle borders.shp") 
+ugs <- st_read("Data/Green_space.shp") # MasterMap topographic polygon
+
+## Build 1x1km grid with city extension NB: entire city territory has been considered (urban+agricultural)
+# Get the bounding box of the shapefile
+bbox <- st_bbox (city_border)
+#grid of 1x1 km
+grid <- st_sf(geometry = st_make_grid (bbox, cellsize = c(1000, 1000),n =, what = "polygons"))
+st_crs (grid)
+grid$id <- 1:224 #add column id
+
+#Plot grid
+plot(grid)
+ggplot() +
+  geom_sf(data = city_border) +
+  geom_sf(data = grid, color = "blue", fill = NA) +
+  theme_minimal()
+
+# be sure both shp are in the same crs
+st_crs(grid) == st_crs (ugs)
+grid <- st_transform(grid, st_crs(ugs))
+
+##############
+### built database
+##############
+
+# clip grid with city border as mask
+grid_clip <- st_intersection(grid, city_border) [,1] #select ID column only
+
+#plot
+ggplot() +
+  geom_sf(data = city_border) +
+  geom_sf(data = grid_clip, color = "blue", fill = NA) +
+  theme_minimal()
+
+# intersect ugs with the grid
+ugs_grid <- st_intersection(ugs, grid_clip)
+
+# Calculate clip_grid area (= area cell) and ugs area
+grid_clip$area_cels <- as.vector (st_area(grid_city))
+ugs_grid$ugs_area <-as.vector(st_area(ugs_grid))
+
+#plot
+ggplot() +
+  geom_sf(data = city_border) +
+  geom_sf(data = grid_city, color = "blue", fill = NA) +
+  theme_minimal()
+
+#extract attribute table and save in csv
+write.csv(st_drop_geometry(ugs_grid),file.path= "Data/ugs_data.csv",row.names=F) 
+
+
+##############
+### calculate ugs coverage
+##############
+
+# import data
+ugs_data <- read.csv("Data/ugs_data.csv", sep=",")
+
+#sum ugs area per cell
+unique(ugs_data$secForm)
+length(tot_ugs_cell)
+length(unique(ugs_data$id))
+length (ugs_data$ugs_area)
+
+area_ugs_cell <- tapply(ugs_data$ugs_area, ugs_data$id, FUN=sum)
+
+#area cell
+length(grid_city$area_cels)
+# Filter the data frame based on the "id" values
+grid_study <- grid_city[grid_city$id %in% unique(ugs_data$id), ]
+
+# Calculate the percentage of coverage
+ugs_coverage <- (area_ugs_cell / grid_study$area_cels) * 100
+
+#add ugs coverage to grid 
+grid_study$ugs_coverage <- as.vector(ugs_coverage)
+
+##############
+### model
+##############
+
+hive_scenario <- grid_study
+
+# different cc scenario 
+# formula -> 100: cc= tot_ugs:hive1
+hive_scenario$hive1 <- (hive_scenario$ugs_coverage*1/100) # cc=1
+hive_scenario$hive2 <- (hive_scenario$ugs_coverage*4 /100) # cc=4
+hive_scenario$hive3 <- (hive_scenario$ugs_coverage*7.5 /100) # cc=7.5
+hive_scenario$hive4 <- (hive_scenario$ugs_coverage*10 /100) # cc=10
+
+st_write(hive_scenario, "Output/Scenario 1", driver = "ESRI Shapefile")
+
+Scenario1_data <- st_drop_geometry(hive_scenario)
+write.csv(Scenario1_data,file= "Output/Scenario1_data.csv",row.names=F)
+
+getwd()
+
